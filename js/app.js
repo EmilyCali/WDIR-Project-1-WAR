c134c35b0fb98e5d54f812f6559dc49684ac2254
emilycali <cali_emily@bah.com>
2017-03-26 13:18:55 -0400

    Commit 3 - psuedocoding and general html layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
 console.log("CONNECTED!")
+
+$(function() {
+
+//grab elements
+
+
+//grab the initial deck div
+//array of entire deck
+//two empty arrays for when the cards are split
+
+//player 1 hand (new div)
+//player 2 hand (new div)
+
+// handlers
+
+//random split of deck making two new divs that have the resulting arrays in them
+    //math random
+    //push half to one hand
+    //push the other to the other hand
+
+//turns
+  //on click show card
+  // create new divs for the cards to move to
+    //compare cards
+      //if players card is more than other players card
+      //if tie make pick more cards
+        //move compared cards to new arrays (higher value goes to the discard of the person with the winning card)
+
+    // go to other player and repeat above
+
+//check win for rounds
+
+//check win for game
+
+//restart game
+
+// keep score
+
+
+//listeners
+
+//first deck div on click run split
+
+//two new decks on click run turns
+
+//restart run restart
+
+
+})
