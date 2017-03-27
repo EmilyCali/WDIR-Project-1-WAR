e6c2f40ee7592e8cbf7674bccb36e988b2bde5be
emilycali <cali_emily@bah.com>
2017-03-27 18:14:29 -0400

    Commit 8 - all sources for images written into js

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
 //console.log("CONNECTED!")
 var deck = $("#first-deck");
 
-var starterDeck = [{name: "A", suit: "hearts", value: 1, src: ""},
-                  {name: "A", suit: "diamonds", value: 1, src: ""},
-                  {name: "A", suit: "clubs", value: 1, src: ""},
-                  {name: "A", suit: "spades", value: 1, src: ""},
+var starterDeck = [{name: "A", suit: "hearts", value: 1, src: "images/ace_of_hearts.png"},
+                  {name: "A", suit: "diamonds", value: 1, src: "images/ace_of_diamonds.png"},
+                  {name: "A", suit: "clubs", value: 1, src: "images/ace_of_clubs.png"},
+                  {name: "A", suit: "spades", value: 1, src: "images/ace_of_spades.png"},
 
-                  {name: "2", suit: "hearts", value: 2, src: ""},
-                  {name: "2", suit: "diamonds", value: 2, src: ""},
-                  {name: "2", suit: "clubs", value: 2, src: ""},
-                  {name: "2", suit: "spades", value: 2, src: ""},
+                  {name: "2", suit: "hearts", value: 2, src: "images/2_of_hearts.png"},
+                  {name: "2", suit: "diamonds", value: 2, src: "images/2_of_diamonds.png"},
+                  {name: "2", suit: "clubs", value: 2, src: "images/2_of_clubs.png"},
+                  {name: "2", suit: "spades", value: 2, src: "images/2_of_spades.png"},
 
-                  {name: "3", suit: "hearts", value: 3, src: ""},
-                  {name: "3", suit: "diamonds", value: 3, src: ""},
-                  {name: "3", suit: "clubs", value: 3, src: ""},
-                  {name: "3", suit: "spades", value: 3, src: ""},
+                  {name: "3", suit: "hearts", value: 3, src: "images/3_of_hearts.png"},
+                  {name: "3", suit: "diamonds", value: 3, src: "images/3_of_diamonds.png"},
+                  {name: "3", suit: "clubs", value: 3, src: "images/3_of_clubs.png"},
+                  {name: "3", suit: "spades", value: 3, src: "images/3_of_spades.png"},
 
-                  {name: "4", suit: "hearts", value: 4, src: ""},
-                  {name: "4", suit: "diamonds", value: 4, src: ""},
-                  {name: "4", suit: "clubs", value: 4, src: ""},
-                  {name: "4", suit: "spades", value: 4, src: ""},
+                  {name: "4", suit: "hearts", value: 4, src: "images/4_of_hearts.png"},
+                  {name: "4", suit: "diamonds", value: 4, src: "images/4_of_diamonds.png"},
+                  {name: "4", suit: "clubs", value: 4, src: "images/4_of_clubs.png"},
+                  {name: "4", suit: "spades", value: 4, src: "images/4_of_spades.png"},
 
-                  {name: "5", suit: "hearts", value: 5, src: ""},
-                  {name: "5", suit: "diamonds", value: 5, src: ""},
-                  {name: "5", suit: "clubs", value: 5, src: ""},
-                  {name: "5", suit: "spades", value: 5, src: ""},
+                  {name: "5", suit: "hearts", value: 5, src: "images/5_of_hearts.png"},
+                  {name: "5", suit: "diamonds", value: 5, src: "images/5_of_diamonds.png"},
+                  {name: "5", suit: "clubs", value: 5, src: "images/5_of_clubs.png"},
+                  {name: "5", suit: "spades", value: 5, src: "images/5_of_spades.png"},
 
-                  {name: "6", suit: "hearts", value: 6, src: ""},
-                  {name: "6", suit: "diamonds", value: 6, src: ""},
-                  {name: "6", suit: "clubs", value: 6, src: ""},
-                  {name: "6", suit: "spades", value: 6, src: ""},
+                  {name: "6", suit: "hearts", value: 6, src: "images/6_of_hearts.png"},
+                  {name: "6", suit: "diamonds", value: 6, src: "images/6_of_diamonds.png"},
+                  {name: "6", suit: "clubs", value: 6, src: "images/6_of_clubs.png"},
+                  {name: "6", suit: "spades", value: 6, src: "images/6_of_spades.png"},
 
-                  {name: "7", suit: "hearts", value: 7, src: ""},
-                  {name: "7", suit: "diamonds", value: 7, src: ""},
-                  {name: "7", suit: "clubs", value: 7, src: ""},
-                  {name: "7", suit: "spades", value: 7, src: ""},
+                  {name: "7", suit: "hearts", value: 7, src: "images/7_of_hearts.png"},
+                  {name: "7", suit: "diamonds", value: 7, src: "images/7_of_diamonds.png"},
+                  {name: "7", suit: "clubs", value: 7, src: "images/7_of_clubs.png"},
+                  {name: "7", suit: "spades", value: 7, src: "images/7_of_spades.png"},
 
-                  {name: "8", suit: "hearts", value: 8, src: ""},
-                  {name: "8", suit: "diamonds", value: 8, src: ""},
-                  {name: "8", suit: "clubs", value: 8, src: ""},
-                  {name: "8", suit: "spades", value: 8, src: ""},
+                  {name: "8", suit: "hearts", value: 8, src: "images/8_of_hearts.png"},
+                  {name: "8", suit: "diamonds", value: 8, src: "images/8_of_diamonds.png"},
+                  {name: "8", suit: "clubs", value: 8, src: "images/8_of_clubs.png"},
+                  {name: "8", suit: "spades", value: 8, src: "images/8_of_spades.png"},
 
-                  {name: "9", suit: "hearts", value: 9, src: ""},
-                  {name: "9", suit: "diamonds", value: 9, src: ""},
-                  {name: "9", suit: "clubs", value: 9, src: ""},
-                  {name: "9", suit: "spades", value: 9, src: ""},
+                  {name: "9", suit: "hearts", value: 9, src: "images/9_of_hearts.png"},
+                  {name: "9", suit: "diamonds", value: 9, src: "images/9_of_diamonds.png"},
+                  {name: "9", suit: "clubs", value: 9, src: "images/9_of_clubs.png"},
+                  {name: "9", suit: "spades", value: 9, src: "images/9_of_spades.png"},
 
-                  {name: "10", suit: "hearts", value: 10, src: ""},
-                  {name: "10", suit: "diamonds", value: 10, src: ""},
-                  {name: "10", suit: "clubs", value: 10, src: ""},
-                  {name: "10", suit: "spades", value: 10, src: ""},
+                  {name: "10", suit: "hearts", value: 10, src: "images/10_of_hearts.png"},
+                  {name: "10", suit: "diamonds", value: 10, src: "images/10_of_diamonds.png"},
+                  {name: "10", suit: "clubs", value: 10, src: "images/10_of_clubs.png"},
+                  {name: "10", suit: "spades", value: 10, src: "images/10_of_spades.png"},
 
-                  {name: "J", suit: "hearts", value: 11, src: ""},
-                  {name: "J", suit: "diamonds", value: 11, src: ""},
-                  {name: "J", suit: "clubs", value: 11, src: ""},
-                  {name: "J", suit: "spades", value: 11, src: ""},
+                  {name: "J", suit: "hearts", value: 11, src: "images/jack_of_hearts.png"},
+                  {name: "J", suit: "diamonds", value: 11, src: "images/jack_of_diamonds.png"},
+                  {name: "J", suit: "clubs", value: 11, src: "images/jack_of_clubs.png"},
+                  {name: "J", suit: "spades", value: 11, src: "images/jack_of_spades.png"},
 
-                  {name: "Q", suit: "hearts", value: 12, src: ""},
-                  {name: "Q", suit: "diamonds", value: 12, src: ""},
-                  {name: "Q", suit: "clubs", value: 12, src: ""},
-                  {name: "Q", suit: "spades", value: 12, src: ""},
+                  {name: "Q", suit: "hearts", value: 12, src: "images/queen_of_hearts.png"},
+                  {name: "Q", suit: "diamonds", value: 12, src: "images/queen_of_diamonds.png"},
+                  {name: "Q", suit: "clubs", value: 12, src: "images/queen_of_clubs.png"},
+                  {name: "Q", suit: "spades", value: 12, src: "images/queen_of_spades.png"},
 
-                  {name: "K", suit: "hearts", value: 13, src: ""},
-                  {name: "K", suit: "diamonds", value: 13, src: ""},
-                  {name: "K", suit: "clubs", value: 13, src: ""},
-                  {name: "K", suit: "spades", value: 13, src: ""}];
+                  {name: "K", suit: "hearts", value: 13, src: "images/king_of_hearts.png"},
+                  {name: "K", suit: "diamonds", value: 13, src: "images/king_of_diamonds.png"},
+                  {name: "K", suit: "clubs", value: 13, src: "images/king_of_clubs.png"},
+                  {name: "K", suit: "spades", value: 13, src: "images/king_of_spades.png"}];
 
 var $handOne = [];
 var $handTwo = [];
@@ -92,8 +92,8 @@ var deck = $("#first-deck");
       //hides the initial deck div
       deck.hide();
       //creates the two hands
-      var playerOneHand = $("<div />").attr("id", "player-one").appendTo("#board");
-      var playerTwoHand = $("<div />").attr("id", "player-two").appendTo("#board");
+      var $playerOneHand = $("<div />").attr("id", "player-one").appendTo("#board");
+      var $playerTwoHand = $("<div />").attr("id", "player-two").appendTo("#board");
       //math random to shuffle deck
       var cards = [];
       for (var i = 0; i < starterDeck.length; i++) {
@@ -112,21 +112,40 @@ var deck = $("#first-deck");
         //console.log($handTwo);
     };
 //starts the game
-deck.on("click", onClickSplitDeck);
+    deck.on("click", onClickSplitDeck);
 
 //turns
-    var onClickShowCard = function() {
+    var onClickShowCardOne = function() {
       //on click show card
+      var playerOneHand = $("#player-one");
+      for (var i = 0; i < $handOne.length; i++) {
+        $handOne[i].src.appendTo(playerOneHand);
 
+      }
         //compare cards
           //if players card is more than other players card
           //if tie make pick more cards
             //move compared cards to new arrays (higher value goes to the discard of the person with the winning card)
     };
 
-
+  $("#player-one").on("click", onClickShowCardOne)
 
     // go to other player and repeat above
+
+    var onClickShowCardTwo = function() {
+      //on click show card
+      var playerTwoHand = $("#player-two");
+      for (var i = 0; i < $handTwo.length; i++) {
+        $handTwo[i].src.appendTo(playerTwoHand);
+
+      }
+        //compare cards
+          //if players card is more than other players card
+          //if tie make pick more cards
+            //move compared cards to new arrays (higher value goes to the discard of the person with the winning card)
+    };
+
+  $("#player-two").on("click", onClickShowCardTwo)
 
 //check win for rounds
 
